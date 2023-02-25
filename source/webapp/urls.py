467316555d196bc95c0e9fb7e3fc0dcfb7f52578
Ilkham Baudinov <ilkhambaudinov@gmail.com>
2023-02-25 16:33:34 +0600

    Добавил возможность редактирования каждого гостя, прописал все пути, все работает корректно, добавил папку partial сделал общий шаблон для добавления и для редактирования

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from webapp.view.guest import guest_create
+from webapp.view.guest import guest_create, guest_edit
 from webapp.view.index import index_view
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('create_guest/', guest_create, name='create_guest'),
+    path('guest/<int:pk>/edit/', guest_edit, name='edit_guest'),
 ]
--- source/webapp/view/guest.py
+++ source/webapp/view/guest.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from webapp.forms import GuestForms
 from webapp.models import GuestBook
@@ -18,3 +18,23 @@ def guest_create(request):
     else:
         form = GuestForms()
     return render(request, 'guest_create.html', {'form': form})
+
+
+def guest_edit(request, pk):
+    guest = get_object_or_404(GuestBook, pk=pk)
+    if request.method == 'POST':
+        form = GuestForms(request.POST)
+        if form.is_valid():
+            guest.name = form.cleaned_data['name']
+            guest.email = form.cleaned_data['email']
+            guest.text = form.cleaned_data['text']
+            guest.save()
+            return redirect('index')
+    else:
+        guest_data = {
+            'name': guest.name,
+            'email': guest.email,
+            'text': guest.text,
+        }
+        form = GuestForms(initial=guest_data)
+    return render(request, 'edit_guest.html', {'form': form})
